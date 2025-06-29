b56717e2b42d000102852d172a88207e9fd8dbb0
NazrilPrananda <nazrilprananda@gmail.com>
2025-06-29 21:27:01 +0700

    Update Tiktok

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="mt-4 md:mt-0 space-x-4">
             <a href="mailto:smkletris2020@gmail.com" class="hover:underline">Email</a>
-            <a href="https://www.tiktok.com/@letrisindonesia2official" class="hover:underline">tiktok</a>
+            <a href="https://www.tiktok.com/@letrisindonesia2official" class="hover:underline">Tiktok</a>
             <a href="https://www.instagram.com/letrisindonesia2official?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="hover:underline">Instagram</a>
             <a href="https://www.youtube.com/@letrisIndonesia2official" class="hover:underline">YouTube</a>
         </div>
